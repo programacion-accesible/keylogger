4affd7dfcaa23a0fb2c0ea1886b0715be353902a
GerardKessler <gera.kessler@gmail.com>
2024-02-02 23:51:09 -0300

    version inicial

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # keylogger
+
+Gerardo Kessler <gera.ar@yahoo.com>
+
+Pequeño módulo global que permite obtener las estadísticas de las teclas alfanuméricas pulsadas por el usuario.
+
+Los gestos pueden asignarse desde el apartado gestos de entrada, categoría keylogger
+
+Se utiliza la plantilla de complementos desde el repositorio [addon template](https://github.com/nvdaaddons/AddonTemplate)
+
+Hay que modificar el archivo buildVars.py con los datos como está especificado en el readme del repositorio de la plantilla.
+
+Para complilar el complemento es necesario contar con las librerías de python scons y markdown. NVDA utiliza python 3.7.9 en la versión 2023 de NVDA, y python 3.11.6 en la versión 2024
+
+Es conveniente asimismo agregar la ruta de la carpeta bin del programa de traducciones poedit previamente instalado a las variables de entorno, por ejemplo; C:\Program Files (x86)\Poedit\GettextTools\bin
+
+El comando scons aplicado en la raíz de este repositorio genera el archivo compilado en formato nvda-addon
---
+++ addon/globalPlugins/keylogger/__init__.py
@@ -0,0 +1,78 @@
+﻿# Gerardo Kessler (http://gera.ar)
+
+import globalPluginHandler
+from scriptHandler import script
+import ui
+
+# Clase que define métodos de eventos y scripts, asociaciones de gestos y otro código
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	
+	# Variable de clase que aloja un diccionario vacío donde se almacenan las teclas capturadas
+	keys= {}
+	# Variable de control para activar o desactivar la captura
+	toggle= False
+
+	# Método que recibe todos los eventos de teclado
+	def getScript(self, gesture):
+		# Obtenemos el nombre de la tecla pulsada
+		name= _(gesture.mainKeyName)
+		# Si la variable de control está activa se verifica si la tecla capturada existe en el diccionario para sumar el valor, o agregarla como nuevo elemento
+		if self.toggle:
+			if name in self.keys:
+				self.keys[name] += 1
+			# Solo guardamos si es número o letra
+			elif len(name) == 1 and name.isalpha() or name.isdigit():
+				self.keys[name]= 1
+		# Retornamos el evento de teclado para que se ejecute
+		return globalPluginHandler.GlobalPlugin.getScript(self, gesture)
+
+	# Decorador donde se define la categoría en los gestos de entrada, la descripción y el atajo de teclado
+	@script(
+		category= 'keylogger',
+		description= 'Conmuta la activación de la captura de teclas',
+		gesture= None
+	)
+	def script_toggle(self, gesture):
+		if not self.toggle:
+			self.toggle= True
+			# Enviamos el mensaje para que sea verbalizado por el sintetizador
+			ui.message('Capturando las teclas')
+		else:
+			self.toggle= False
+			ui.message('Captura de teclas pausada')
+
+	@script(
+		category= 'keylogger',
+		description= 'Muestra una ventana con los resultados temporales de la captura',
+		gesture= None
+	)
+	def script_show(self, gesture):
+		# Obtenemos una lista de tuplas ordenadas de mayor a menor por los valores del diccionario
+		sorted_items= sorted(
+			self.keys.items(),
+			key=lambda x: x[1],
+			reverse=True
+		)
+		# Concatenamos los elementos de la lista utilizando el salto de línea como delimitador
+		string= '\n'.join([
+			'{}: {}'.format(
+				i[0], i[1]
+			) for i in sorted_items
+		])
+		# Obtenemos la suma de los valores y concatenamos con la cadena anterior
+		string= 'Total de letras y números capturados: {}\n{}'.format(
+			sum(e[1] for e in self.keys.items()),
+			string
+		)
+		# Ventana de diálogo propia de NVDA para mostrar los resultados
+		ui.browseableMessage(string, 'Resultados de captura')
+
+	@script(
+		category= 'keylogger',
+		description= 'Resetea el historial de capturas',
+		gesture= None
+	)
+	def script_resetList(self, gesture):
+		# Vaciamos el diccionario
+		self.keys.clear()
+		ui.message('Lista de teclas reseteada')
